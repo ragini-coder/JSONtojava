37462125d14b76d538fec40f40ce16f4855f5065
ragini-coder <ragini@adstringo.in>
2019-07-05 17:03:13 +0530

    Initial commit

---
+++ app/src/main/java/com/example/jsontojava/JSONResponse.java
@@ -0,0 +1,10 @@
+package com.example.jsontojava;
+
+public class JSONResponse {
+
+    private AndroidVersion[] android;
+
+    public AndroidVersion[] getAndroid() {
+        return android;
+    }
+}
